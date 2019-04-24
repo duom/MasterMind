528f1cf2008109f80a77839af90653d4e98c6437
Albert <albertfernandezgarcia@gmail.com>
2019-04-24 22:36:14 +0200

    primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println( "**********MASTERMIND************");
+        System.out.println("");
+        Game game = new Game();
+
+        game.play();
+
+
+
+    }
+}
